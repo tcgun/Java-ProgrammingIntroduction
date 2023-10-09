94d9ae3a01dc71bfdd851c52f2fe30467c1ff6cf
tcgun <129428964+tcgun@users.noreply.github.com>
2023-10-09 12:23:45 +0300

    Working with Primitive Data Types

---
+++ 3_dataTypes.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double sayi = 12.5; // Double türünde bir değişken tanımlandı ve 12.5 değeri atandı
+
+        sayi = 1414; // Varolan double değişkeninin değeri 1414 ile güncellendi
+
+        char karakter = 'A'; // Char türünde bir değişken tanımlandı ve 'A' karakteri atandı
+
+        boolean dogruMu = false; // Boolean türünde bir değişken tanımlandı ve false değeri atandı
+    }
+}
