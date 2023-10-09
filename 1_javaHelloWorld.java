de636281e51922ea2ba5585caac17f71a6dfa64f
tcgun <129428964+tcgun@users.noreply.github.com>
2023-10-09 12:22:17 +0300

    Let's start with a 'Hello, World!' application

---
+++ 1_javaHelloWorld.java
@@ -0,0 +1,10 @@
+public class Main {
+    // Bu satır, "Main" adlı bir Java sınıfını tanımlar.
+
+    public static void main(String[] args) {
+        // Bu satır, Java programının giriş noktasını temsil eder. Her Java uygulamasının bir "main" metoduna ihtiyacı vardır.
+
+        System.out.println("Merhaba Java");
+        // Bu satır, ekrana "Merhaba Java" metnini yazdırır. System.out.println metodu, bir metni ekrana bastırmak için kullanılır.
+    }
+}
