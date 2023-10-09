64da0df35eaebf858998f4c14c359c64d41e81aa
tcgun <129428964+tcgun@users.noreply.github.com>
2023-10-09 14:30:11 +0300

    Working with a For Loop

---
+++ 7_forLoop.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //For
+
+        // "i" değişkeni 2'den başlayarak başlar.
+        // Döngü, "i" değişkenini 10'dan küçük olduğu sürece çalışır.
+        // Her adımda "i" değişkeni 2 artar.
+        for(int i=2; i<10; i+=2){
+            // "i" değişkeninin değerini ekrana yazdırır.
+            System.out.println(i);
+        }
+
+        // Döngü tamamlandığında, "For Döngüsü Bitti" mesajını yazdırır.
+        System.out.println("For Döngüsü Bitti");
+    }
+}
